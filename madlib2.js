859671142dfb3cb046edceb1d471ef7ad68a1f4b
MackenzieM17 <32307291+MackenzieM17@users.noreply.github.com>
2017-10-11 09:13:02 -0400

    Continuing the program

--- madlib2.js
+++ madlib2.js
@@ -1,17 +1,26 @@
 //Initialize Variables
-var sentence = [];
-var replacements = [];
-var index = 0;
-filler(sentence, "Next Word:" );
-filler(replacements, "Next Position:");
+var sentence = []; //telling the program the variable
+var replacements = []; //telling the program the variable
+var index = 0; //telling the program the variable
+filler(sentence, "Next Word:" ); 
+filler(replacements, "Next Position:"); //what the program is replacing
 
-
+replace(sentence,replacements,"Part of Speech for word in sentence");
+alert("Switch Positions");
 function filler (myArray,myPrompt){
-index = -1;
-do{
-	index++;
-	myArray[index] = prompt(myPrompt +index+ ":");
+	index = -1;
+	do{
+		index++;
+		myArray[index] = prompt(myPrompt +"["+index+"]=");
+	}
+	while(myArray[index] != "^");
+	myArray.pop();
+	console.log(myArray);
 }
-while(myArray[index] != "^");
-console.log(myArray);
+
+function replace (mySArray, myRArray, myPrompt){
+	for (index = 0; index < myRArray.length;index++){
+	mySArray[myRArray[index]] = prompt(myPrompt+"["+[myRArray[index]]+"] =");
+	}
+	console.log(mySArray);
 }
